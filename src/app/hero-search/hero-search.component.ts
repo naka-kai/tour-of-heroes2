aceb5cef5646c1de19ed6bea25cba2c53da9790c
kai <sea.k.naka@gmail.com>
2022-12-23 19:44:02 +0900

    finish

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
   styleUrls: ['./hero-search.component.css']
 })
-export class HeroSearchComponent {
+export class HeroSearchComponent implements OnInit {
+  heroes$!: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
 
+  constructor(private heroService: HeroService) { }
+
+  // 検索語をobservableストリームにpushする
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms
+      .pipe(
+        // 各キーストロークの後、検索前に300ms待つ
+        debounceTime(300),
+
+        // 直前の検索語と同じ場合は無視する
+        distinctUntilChanged(),
+
+        // 検索語が変わる度に、新しいobservableにスイッチする
+        switchMap((term: string) => this.heroService.searchHeroes(term)),
+      );
+  }
 }
--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -3,7 +3,15 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class MessageService {
 
-  constructor() { }
+export class MessageService {
+  messages: string[] = [];
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
 }
